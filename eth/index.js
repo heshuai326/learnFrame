4f2ed066469d7c6e1af52be70c6c9a407b49b27f
何帅 <>
2018-11-05 20:45:30 +0800

    提交

--- eth/index.js
+++
@@ -1,5 +0,0 @@
-var api = require('etherscan-api').init('DNG9YWZ6U44Z3V1PQQHMNUAAW241Z9RP28');
-var balance = api.account.balance('0x65e7Fd5479beB0D11C0073259E22809E2e3BEa6e');
-balance.then(function(balanceData){
-  console.log(balanceData);
-});
