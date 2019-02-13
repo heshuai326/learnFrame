4d946454b29c60cf99112275771e7571be462195
heshuai326 <2313843501@qq.com>
2019-02-13 12:16:08 +0800

    执行环境

---
+++ 语言/ES5/1/1-1/index.js
@@ -0,0 +1,21 @@
+console.log(fun)
+
+console.log(person)
+
+console.log(person)
+
+console.log(fun)
+
+var person = "Eric";
+
+console.log(person)
+
+function fun() {
+    console.log(person)
+    var person = "Tom";
+    console.log(person)
+}
+
+fun()
+
+console.log(person)
